46f367342acf3918f9c2c226764b42166e61b9d9
John Pribesh <beopensourcellc@gmail.com>
2013-12-05 12:44:16 -0500

    updating picmap.js

--- js/picMap.js
+++ js/picMap.js
@@ -1,17 +1,18 @@
 // nycsubwayphotomap
 var stations = [
-		{ name: "242st", loc: "276,508,34"}, 
-		{ name: "238st", loc: "286,560,20"}, 
-		{ name: "231st", loc: "286,595,15"}
+		{ line: "Red-line", name: "242st", loc: "276,508,34"}, 
+		{ line: "Red-line", name: "238st", loc: "286,560,20"}, 
+		{ line: "Red-line", name: "231st", loc: "286,595,15"}
 ];
 
 $(document).ready(function(){
 	for(var i=0; i<stations.length; i++){
 			var loc = stations[i].loc;
 			var name = stations[i].name;
+			var line = stations[i].line;
 			$('#subway').append('<a href="images/' + name + '.jpg"' + 
-				' title="' + name + '" rel="prettyPhoto"' + name + 
-				'"><area shape="circle" coords="' + loc + '" border="1" /></a>');
+				' title="' + line + " " + name + '" rel="prettyPhoto"' + name + '">' +
+				'<area shape="circle" coords="' + loc + '" border="1" /></a>');
 	}
 	$("a[rel^='prettyPhoto']").prettyPhoto();
 })
