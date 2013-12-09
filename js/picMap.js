9fdb936a483a5057d086e6a8427134ae2efe62bf
BeOpenSource <beopensourcellc@gmail.com>
2013-12-09 09:04:53 -0500

    Update picMap.js
    replaced in line 7:
    ' + name + '
    replaced with 242st to test map functionality

--- js/picMap.js
+++ js/picMap.js
@@ -4,7 +4,7 @@ $(document).ready(function(e){
 		for(var i=0; i<stations.length; i++){
 				var loc = stations[i].loc;
 				var name = stations[i].name;
-				$('#subway').append('<a href="images/' + name + '.jpg"' + 
+				$('#subway').append('<a href="images/242st.jpg"' + 
 					' title="'+ name + '" rel="prettyPhoto"' + name + '">' +
 					'<area shape="circle" coords="' + loc + '"/></a>');
 		}
