08f0c74db99f319f5933dd41024c7f2b9fd87cb1
BeOpenSource <beopensourcellc@gmail.com>
2013-12-09 08:52:48 -0500

    Update picMap.js
    added get script function to include array

--- js/picMap.js
+++ js/picMap.js
@@ -1,13 +1,15 @@
 // nycsubwayphotomap
 $(document).ready(function(e){
-	for(var i=0; i<stations.length; i++){
-			var loc = stations[i].loc;
-			var name = stations[i].name;
-			$('#subway').append('<a href="images/' + name + '.jpg"' + 
-				' title="'+ name + '" rel="prettyPhoto"' + name + '">' +
-				'<area shape="circle" coords="' + loc + '"/></a>');
-	}
-	$("a[rel^='prettyPhoto']").prettyPhoto();
-	$('img[usemap]').rwdImageMaps();
+	$.getScript('js/stations.js', function() {
+		for(var i=0; i<stations.length; i++){
+				var loc = stations[i].loc;
+				var name = stations[i].name;
+				$('#subway').append('<a href="images/' + name + '.jpg"' + 
+					' title="'+ name + '" rel="prettyPhoto"' + name + '">' +
+					'<area shape="circle" coords="' + loc + '"/></a>');
+		}
+		$("a[rel^='prettyPhoto']").prettyPhoto();
+		$('img[usemap]').rwdImageMaps();
+	});
 })
 
